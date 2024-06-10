f4b8189cf713570d9da203e413769ad6434af1be
“Jessica <osias.jessica@gmail.comgit config --global user.email osias.jessica@gmail.com>
2024-06-10 19:57:44 -0400

    Initial Commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MadLib(); 
+        }
+
+        public static void MadLib()
+        {
+
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine ($"Hello, {userName}! What is your favorite color?");
+            string favColor = Console.ReadLine();
+            Console.WriteLine("Favorite band?");
+            string favBand = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string favAnimal = Console.ReadLine();
+
+            Console.WriteLine($"One day {userName}, was walking in the woods while wearing a {favColor} t-shirt. {userName} saw a {favAnimal} while listening to {favBand}.");
         }
     }
 }
